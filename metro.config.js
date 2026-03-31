f5d755a240b5acde370e195ef82d43c445e725a2
FrancoisCoding <FrancoisCoding@users.noreply.github.com>
2026-03-31 08:21:01 -0400

    fix: stabilize expo web startup

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,12 @@
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = getDefaultConfig(__dirname);
+function normalizeProjectRoot(projectRoot) {
+  if (process.platform !== "win32") {
+    return projectRoot;
+  }
+
+  return projectRoot.replace(/^[a-z]:/, (drive) => drive.toUpperCase());
+}
+
+module.exports = getDefaultConfig(normalizeProjectRoot(path.resolve(__dirname)));
